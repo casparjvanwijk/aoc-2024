77d5ab5aacf283de3c8315b4d457b3384d57adc3
Caspar Van Wijk <caspar@wecodegreen.com>
2025-09-22 17:28:59 +0200

    Refactor day 15

--- 15-2.go
+++ 15-2.go
@@ -15,7 +15,11 @@ type co struct {
 	x, y int
 }
 
-var moveBySymbol = map[rune]co{
+type move struct {
+	x, y int
+}
+
+var moveBySymbol = map[rune]move{
 	'^': {0, -1},
 	'>': {1, 0},
 	'v': {0, 1},
@@ -30,141 +34,87 @@ func (b box) fields() []co {
 	return []co{b.pos, co{b.pos.x + 1, b.pos.y}}
 }
 
-type grid struct {
+type game struct {
 	width, height int
 	walls         map[co]struct{}
 	robot         co
-	boxes         map[co]*box
+	boxes         []*box
+	// boxMap keeps track of the boxes indexed by position for fast lookup.
+	boxMap map[co]*box
 }
 
-func main() {
-	var (
-		walls = make(map[co]struct{})
-		boxes = make(map[co]*box)
-		robot co
-		moves []co
-	)
-
-	file, _ := os.Open("15-input.txt")
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-
-	// Parse grid.
-	width := utf8.RuneCountInString(scanner.Text()) * 2
-	y := 0
-	for ; scanner.Text() != ""; y++ {
-		for i, rune := range scanner.Text() {
-			x := 2 * i
-			switch rune {
-			case '@':
-				robot = co{x, y}
-			case '#':
-				walls[co{x, y}], walls[co{x + 1, y}] = struct{}{}, struct{}{}
-			case 'O':
-				box := box{co{x, y}}
-				boxes[co{x, y}], boxes[co{x + 1, y}] = &box, &box
-			default:
-				continue
-			}
-		}
-		scanner.Scan()
+func newGame(width, height int, walls map[co]struct{}, robot co, boxes []*box) game {
+	g := game{
+		width:  width,
+		height: height,
+		walls:  walls,
+		robot:  robot,
+		boxes:  boxes,
 	}
-	height := y
-
-	// Parse moves.
-	for scanner.Scan() {
-		line := scanner.Text()
-		for _, s := range line {
-			moves = append(moves, moveBySymbol[s])
-		}
-	}
-
-	g := grid{width, height, walls, robot, boxes}
-
-	g.paint()
-	time.Sleep(delay)
-	for _, m := range moves {
-		g.update(m)
-		g.paint()
-		time.Sleep(delay)
-	}
-
-	// Calculate answer.
-	uniqueBoxes := make(map[*box]struct{})
-	for _, box := range g.boxes {
-		uniqueBoxes[box] = struct{}{}
-	}
-	result := 0
-	for box, _ := range uniqueBoxes {
-		result += box.pos.x + 100*box.pos.y
-	}
-	fmt.Println(result)
+	g.refreshBoxMap()
+	return g
 }
 
-func (g *grid) pushBox(b *box, d co) bool {
-	boxesChecked := make(map[*box]struct{})
-
-	var canPush func(b *box, d co) bool
-	canPush = func(b *box, d co) bool {
-		boxesChecked[b] = struct{}{}
-		bNext := *b
-		bNext.pos = co{b.pos.x + d.x, b.pos.y + d.y}
-
-		for _, f := range bNext.fields() {
-			if _, ok := g.walls[f]; ok {
-				return false
-			}
-		}
-		for _, f := range bNext.fields() {
-			otherBox, ok := g.boxes[f]
-			if !ok || otherBox == b {
-				continue
-			}
-			_, ok = boxesChecked[otherBox]
-			if ok {
-				continue
-			}
-			if !canPush(otherBox, d) {
-				return false
-			}
-		}
-		return true
-	}
-	if !canPush(b, d) {
-		return false
-	}
-
-	// Move the boxes.
-	for b, _ := range boxesChecked {
+func (g *game) refreshBoxMap() {
+	g.boxMap = make(map[co]*box)
+	for _, b := range g.boxes {
 		for _, f := range b.fields() {
-			delete(g.boxes, f)
-		}
-		b.pos.x += d.x
-		b.pos.y += d.y
-	}
-	for b, _ := range boxesChecked {
-		for _, f := range b.fields() {
-			g.boxes[f] = b
+			g.boxMap[f] = b
 		}
 	}
-	return true
 }
 
-func (g *grid) update(d co) {
+func (g *game) update(d move) {
 	robotNext := co{g.robot.x + d.x, g.robot.y + d.y}
-
+	// Check for wall collision.
 	if _, ok := g.walls[robotNext]; ok {
 		return
 	}
-	box, ok := g.boxes[robotNext]
-	if ok && !g.pushBox(box, d) {
-		return
+	// Check for box collision.
+	b, ok := g.boxMap[robotNext]
+	if ok {
+		canMove, boxesTouched := g.canMoveBox(b, d)
+		if !canMove {
+			return
+		}
+		for _, b := range boxesTouched {
+			b.pos.x += d.x
+			b.pos.y += d.y
+		}
+		g.refreshBoxMap()
 	}
-
 	g.robot = robotNext
 }
 
-func (g *grid) paint() {
+// canMoveBox checks if a box can be moved and also returns all touched boxes.
+func (g *game) canMoveBox(b *box, d move) (canMove bool, boxesTouched []*box) {
+	visited := make(map[*box]struct{})
+	queue := []*box{b}
+
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+
+		if _, ok := visited[cur]; ok {
+			continue
+		}
+		visited[cur] = struct{}{}
+		boxesTouched = append(boxesTouched, cur)
+
+		for _, f := range cur.fields() {
+			fNext := co{f.x + d.x, f.y + d.y}
+			if _, ok := g.walls[fNext]; ok {
+				return false, nil
+			}
+			if boxNext, ok := g.boxMap[fNext]; ok && boxNext != cur {
+				queue = append(queue, boxNext)
+			}
+		}
+	}
+	return true, boxesTouched
+}
+
+func (g *game) paint() {
 	frame := make([][]rune, g.height)
 	for y := range frame {
 		frame[y] = make([]rune, g.width)
@@ -193,6 +143,65 @@ func (g *grid) paint() {
 	}
 	clearScreen()
 	fmt.Print(sb.String())
+}
+
+func main() {
+	var (
+		walls = make(map[co]struct{})
+		boxes = []*box{}
+		robot co
+		moves []move
+	)
+
+	file, _ := os.Open("15-input.txt")
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	// Parse game.
+	width := utf8.RuneCountInString(scanner.Text()) * 2
+	y := 0
+	for ; scanner.Text() != ""; y++ {
+		for i, rune := range scanner.Text() {
+			x := 2 * i
+			switch rune {
+			case '@':
+				robot = co{x, y}
+			case '#':
+				walls[co{x, y}], walls[co{x + 1, y}] = struct{}{}, struct{}{}
+			case 'O':
+				boxes = append(boxes, &box{co{x, y}})
+			default:
+				continue
+			}
+		}
+		scanner.Scan()
+	}
+	height := y
+
+	g := newGame(width, height, walls, robot, boxes)
+
+	// Parse moves.
+	for scanner.Scan() {
+		line := scanner.Text()
+		for _, s := range line {
+			moves = append(moves, moveBySymbol[s])
+		}
+	}
+
+	g.paint()
+	time.Sleep(delay)
+	for _, m := range moves {
+		g.update(m)
+		g.paint()
+		time.Sleep(delay)
+	}
+
+	// Calculate answer.
+	result := 0
+	for _, b := range boxes {
+		result += b.pos.x + 100*b.pos.y
+	}
+	fmt.Println(result)
 }
 
 func clearScreen() {
